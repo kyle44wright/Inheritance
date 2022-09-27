04cfa065f4686d2d4520a19a7810bdeadba04f7f
Kyle <kw@test.com>
2022-09-27 13:42:54 -0500

    more notes

--- f_polymorphism_demo.py
+++ f_polymorphism_demo.py
@@ -9,7 +9,7 @@ def main():
     dog = animals.Dog()
     cat = animals.Cat()
 
-
+'''
     # Display information about each one.
     print('Here are some animals and')
     print('the sounds they make.')
@@ -26,6 +26,20 @@ def main():
 
     cat.show_species()
     cat.make_sound()
+
+'''
+
+    show_mammal_info(gorilla)
+    show_mammal_info(dog)
+    show_mammal_info(cat)
+    show_mammal_info('bird')
+
+def show_mammal_info(creature):
+    if isinstance(creature,animals.Mammal):
+        creature.show_species()
+        creature.make_sound()
+    else:
+        print(creature,"is not a mammal")
 
 # Call the main function.
 main()
